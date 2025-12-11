3fa4086d83100f105a889501612e381b1158c977
PawelSitko <pawelsitko90@gmail.com>
2025-12-11 15:38:23 +0000

    changes

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.HEALTH_HOST || 'localhost',
-  user: process.env.HEALTH_USER || 'root',
-  password: process.env.HEALTH_PASSWORD || '',
-  database: process.env.HEALTH_DATABASE || 'health',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'health',
+
 });
 
 pool.getConnection((err, connection) => {
