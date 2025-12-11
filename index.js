06447c39896464f8cfc35f5baf2222bf935fade2
PawelSitko <pawelsitko90@gmail.com>
2025-12-11 15:22:41 +0000

    Fix index.js (sanitizer + port)

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const app = express();
 const path = require('path');
 const session = require('express-session');
 
-//use the pool from db.js
+// Use the pool from db.js
 const db = require('./db');
 global.db = db;
 
+// Sanitizer
+const expressSanitizer = require('express-sanitizer');
 
 // View engine
 app.set('view engine', 'ejs');
 
 // Body parsing + sanitiser
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 app.use(expressSanitizer());
-
-
 
 // Static files (CSS, images, client JS)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,22 +39,21 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
-// App-level data (like shopName before)
+// App-level globals
 app.locals.appData = { appName: "Health Tracker" };
 
 // ROUTES
 const mainRoutes = require('./routes/main');
 app.use('/', mainRoutes);
 
-// (we'll add these files very soon)
 const userRoutes = require('./routes/users');
 app.use('/users', userRoutes);
 
 const workoutsRoutes = require('./routes/workouts');
 app.use('/workouts', workoutsRoutes);
 
+// PORT (this fixes your VM error)
+const port = process.env.PORT || 8000;
 
 // Start server
 app.listen(port, () => {
